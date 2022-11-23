6bbfe3884ef856bbfb23d04823c665448471ff6f
Kirina Mikhadyuk <mikhadyuk@gmail.com>
2022-11-23 17:05:45 +0300

    changes

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ mongoose.connect(mongoURL);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "*");
+    if (req.method === "OPTIONS") {
+        res.header(
+            "Access-Control-Allow-Methods", "GET,POST,DELETE"
+        )
+        return res.status(200).json({})
+    }
+    next();
+});
+
 app.get("/", (req, res) => {
     res.send("This is customers service");
 });
